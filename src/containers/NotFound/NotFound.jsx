9c80e3a4824eefc149fda35f66d55acee6177a97
xNapha <nathancai1997@gmail.com>
2023-04-25 22:08:49 +1000

    organised the scss/css, and updated NotFound container

--- src/containers/NotFound/NotFound.jsx
+++ src/containers/NotFound/NotFound.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styles from "./NotFound.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-    return <div>NotFound</div>;
+    const navigate = useNavigate();
+    useEffect(() => {
+        setTimeout(() => {
+            navigate("/e-commerce-store/");
+        }, 2000);
+    }, []);
+    return (
+        <div className={styles.Not_Found}>
+            <p>We couldn't find what you were looking for.</p>
+            <p>Sending you back to the home page</p>
+        </div>
+    );
 };
 
 export default NotFound;
